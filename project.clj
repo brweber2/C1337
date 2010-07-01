4ad430575b362db72b0cdde2cc4f2872596f18e9
Bryan Weber <brweber2@yahoo.com>
2010-06-30 23:09:12 -0400

    making it a lein project

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject c1337 "1.0.0-SNAPSHOT"
+  :description "c1337 'cleat' clojure library"
+  :url "http://github.com/brweber2/c1337"
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :repositories {
+                  "maven central" "http://repo1.maven.org/maven2"
+                  "clojure releases" "http://build.clojure.org/releases" 
+                  "clojure snapshots" "http://build.clojure.org/snapshots"
+                  "clojars" "http://clojars.org/repo/"
+                }
+)
+
+
---
+++ test/c1337/core_test.clj
@@ -0,0 +1,6 @@
+(ns c1337.core-test
+  (:use [c1337.core] :reload-all)
+  (:use [clojure.test]))
+
+(deftest replace-me 
+  (is true))
