d7af8d019246ade78aacc972b0f869a6bba6d256
Bryan Weber <brweber2@yahoo.com>
2010-06-30 23:21:32 -0400

    adding more files

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "http://github.com/brweber2/c1337"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :repositories {
-                  "maven central" "http://repo1.maven.org/maven2"
+  :repositories { "maven central" "http://repo1.maven.org/maven2"
                   "clojure releases" "http://build.clojure.org/releases" 
                   "clojure snapshots" "http://build.clojure.org/snapshots"
-                  "clojars" "http://clojars.org/repo/"
-                }
-)
+                  "clojars" "http://clojars.org/repo/" }
+  :dev-dependencies [[lein-clojars "0.5.0"]]
+  :main c1337.core.main )
 
 
